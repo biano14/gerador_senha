6a4ead54476e4fdd25e716a07d5295cfc74ed89a
biano14 <gufaduarte1997@gmail.com>
2024-08-20 17:40:05 -0300

    fix commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import secrets
 while True:
-    confirma = input('Gerar senha? (s/n)').lower()
+    confirma = input('Gerar senha? (s/n): ').lower()
     
     if confirma == 's':
         print(secrets.token_urlsafe(16))
